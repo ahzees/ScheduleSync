2478d712f4fedb6f85d88d0b67f9f4a48ff864da
ahzees <matviy.stets@gmail.com>
2023-02-09 01:56:16 +0200

    chore: added some tests to project [Task]: schedulesync-crudapi-test
    [Description list]
    tests: added some tests to test folder
    
    [Notes]
    <notes>

---
+++ tests/test_main.py
@@ -0,0 +1,52 @@
+import requests
+
+
+class Test:
+
+    def test_module(self):
+        import schedulesync
+
+    def test_import(self):
+        from schedulesync import core
+
+    def test_server(self):
+        url = 'http://127.0.0.1:8000'
+        response = requests.get(url)
+
+        assert response.status_code < 500
+
+    def test_employee_list(self):
+        url = 'http://127.0.0.1:8000/employee'
+        response = requests.get(url)
+
+        assert response.status_code == 200
+
+        response_dict = response.json()
+        assert len(response_dict) != 0
+
+    def test_employee(self):
+        url = 'http://127.0.0.1:8000/employee/1'
+        response = requests.get(url)
+
+        assert response.status_code == 200
+
+        response_dict = response.json()
+        assert len(response_dict) != 0
+
+    def test_occupation_list(self):
+        url = 'http://127.0.0.1:8000/occupation'
+        response = requests.get(url)
+
+        assert response.status_code == 200
+
+        response_dict = response.json()
+        assert len(response_dict) != 0
+
+    def test_occupation(self):
+        url = 'http://127.0.0.1:8000/occupation/1'
+        response = requests.get(url)
+
+        assert response.status_code == 200
+
+        response_dict = response.json()
+        assert len(response_dict) != 0
