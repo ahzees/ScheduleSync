bfda6a9e5e75bb3f5c4818e93e4ed28792d1290d
ahzees <matviy.stets@gmail.com>
2023-02-08 12:27:54 +0200

    chore: refactor # noqa [Task]: schedulesync-fixworkflow
    [Description list]
    fix: added #noqa: C0114
    
    [Notes]
    <notes>

--- schedulesync/__init__.py
+++ schedulesync/__init__.py
@@ -1,2 +1,1 @@
-# noqa: C0114
-__version__ = '0.1.0'
+__version__ = '0.1.0'  # noqa: C0114
--- schedulesync/main.py
+++ schedulesync/main.py
@@ -1,5 +1,4 @@
-# noqa: C0114
-import fastapi
+import fastapi  # noqa: C0114
 
 app = fastapi.FastAPI()
 app()
