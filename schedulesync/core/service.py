0c9947bb0e6a014a5c2ee108766110e44931a3ec
ahzees <matviy.stets@gmail.com>
2023-02-14 04:12:13 +0200

    chore: refactoring occupation`s db calls to async [Task]: schedulesync-auth-2
    [Description list]
    refactor: refactoring database calls to async
    
    [Notes]
    <notes>

--- schedulesync/core/service.py
+++ schedulesync/core/service.py
@@ -1,5 +1,6 @@
 from fastapi import HTTPException
 from sqlalchemy import update
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Session
 
 from schedulesync.core.models.models import Employee, Occupation, Shift
@@ -9,30 +10,6 @@ from schedulesync.core.schemas.schema import Shift as ShiftSchema
 
 def get_item(db, id, table):
     return db.query(table).filter(table.id == id).one()
-
-
-def get_occupation_list(db: Session):
-    return db.query(Occupation).all()
-
-
-def create_occupation(db: Session, item: CreateOccupation):
-    occupation = Occupation(**item.dict())
-    db.add(occupation)
-    db.commit()
-    db.refresh(occupation)
-    return occupation
-
-
-def delete_occupation(db: Session, id: int):
-    db.query(Occupation).filter(
-        Occupation.id == get_item(db, id, Occupation).id
-    ).delete()
-    db.commit()
-    return {}
-
-
-def get_occupation(db: Session, id: int):
-    return get_item(db, id, Occupation)
 
 
 def get_employee_list(db: Session):
--- schedulesync/occupation.py
+++ schedulesync/occupation.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter, Depends
+from fastapi.exceptions import HTTPException
+from sqlalchemy import delete, insert, select
+from sqlalchemy.exc import NoResultFound
 from sqlalchemy.orm import Session
 
+from schedulesync.core.auth.db import get_async_session
+from schedulesync.core.models.models import Occupation
 from schedulesync.core.schemas.schema import CreateOccupation
-from schedulesync.core.service import create_occupation as c_occupation
-from schedulesync.core.service import delete_occupation as d_ocupation
-from schedulesync.core.service import get_occupation as g_occupation
-from schedulesync.core.service import get_occupation_list as g_occupation_list
-from schedulesync.core.utils import get_db
 
 router = APIRouter()
 
@@ -14,25 +14,38 @@ router = APIRouter()
 @router.get(
     "/", status_code=200, summary="Get list of occupation`s", tags=["Occupation"]
 )
-async def get_occupation_list(db: Session = Depends(get_db)):
-    occupation_list = g_occupation_list(db)
-    return occupation_list
+async def get_occupation_list(db: Session = Depends(get_async_session)):
+    occupation_list = select(Occupation)
+    result = await db.execute(occupation_list)
+    return result.scalars().all()
 
 
 @router.get(
     "/{id}", status_code=200, summary="Get specific occupation", tags=["Occupation"]
 )
-async def get_occupation(id: int, db: Session = Depends(get_db)):
-    return g_occupation(id=id, db=db)
+async def get_occupation(id: int, db: Session = Depends(get_async_session)):
+    occupation = select(Occupation).where(Occupation.id == id)
+    result = await db.execute(occupation)
+    try:
+        return result.scalar_one()
+    except NoResultFound:
+        raise HTTPException(status_code=404)
 
 
 @router.post("/", status_code=201, summary="Create occupation", tags=["Occupation"])
-async def create_occupation(item: CreateOccupation, db: Session = Depends(get_db)):
-    return c_occupation(db, item)
+async def create_occupation(
+    item: CreateOccupation, db: Session = Depends(get_async_session)
+):
+    occupation = insert(Occupation).values(**item.dict())
+    await db.execute(occupation)
+    await db.commit()
+    return {"status": "succesful"}
 
 
 @router.delete(
     "/{id}", status_code=200, summary="Delete occupation", tags=["Occupation"]
 )
-async def delete_occupation(id: int, db: Session = Depends(get_db)):
-    return d_ocupation(db, id)
+async def delete_occupation(id: int, db: Session = Depends(get_async_session)):
+    await db.execute(delete(Occupation).where(Occupation.id == id))
+    await db.commit()
+    return {"status": "succescful"}
